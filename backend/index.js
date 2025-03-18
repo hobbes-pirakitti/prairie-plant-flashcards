d2211e48c7c761bd32291e0579c58d16e7c2e99c
hobbes-pirakitti <2792984+hobbes-pirakitti@users.noreply.github.com>
2025-03-18 11:54:37 -0500

    Add data endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
+const jsonDataFileName = "data.json";
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
+});
+
+app.get("/data", (req, res) => {
+    const jsonFilePath = path.join(process.cwd(), jsonDataFileName);
+    res.sendFile(jsonFilePath);
 });
 
 app.listen(port, () => {
