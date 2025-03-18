1ff68abe0ed7993c0378f8d9e07046e4818228b4
hobbes-pirakitti <2792984+hobbes-pirakitti@users.noreply.github.com>
2025-03-18 16:42:58 -0500

    Switch to minimal vite

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const rootNode = document.getElementById('app');
-const root = ReactDOM.createRoot(rootNode);
-
-const heading = React.createElement("h1", {}, "Hello, World!");
-root.render(heading);
