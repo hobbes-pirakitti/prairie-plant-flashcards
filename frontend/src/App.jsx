8b6002aa4b7b0097b66ec48d64f5fd8197ca2441
hobbes-pirakitti <2792984+hobbes-pirakitti@users.noreply.github.com>
2025-03-18 21:02:45 -0500

    Basic filtering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
             const allPlants = [];
             for (const plantGroup of plantGroups) {
                 for (const plant of plantGroup.plants) {
+                    plant.group = plantGroup.group;
                     plant.imageUrl = apiBaseUrl + urijs.joinPaths("/image", plant.image).href();
                     allPlants.push(plant);
                 }
@@ -47,11 +48,28 @@ function App() {
         setPlants(plants.filter(p => p.name !== currentPlant.name));
     }
 
+    function filter(plants, selection){
+        if (selection === "") {
+            showRandom(plants);    
+        }
+        else {
+            showRandom(plants.filter(p => p.group === selection));
+        }
+    }
+
     return <>
             { plants.length > 0 && (
                 <div id="card-wrapper">
                     <button onClick={() => {showRandom(plants);}}>Advance</button>
                     <button onClick={() => {removeCurrent();showRandom(plants);}}>Correct</button>
+                    <select name="groupFilter" onChange={(e) => filter(plants, e.target.value)}>
+                        <option key="None" name="">None ({plants.length})</option>
+                        {
+                            Array.from(new Set(plants.map(p=>p.group))).map(groupName => 
+                                <option key={groupName} value={groupName}>{groupName}&nbsp;({plants.filter(p =>p.group === groupName).length})</option>
+                            )
+                        }
+                    </select>
                     <div className={"plant-info" + (isShowingPlantInfo ? " unhidden" : "")} onClick={showPlantInfo}>
                         <p>{currentPlant.name}</p>
                         <em>{currentPlant.species}</em>
