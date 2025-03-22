5bd7c436fbf581455f6fd019723c18caa54517af
hobbes-pirakitti <2792984+hobbes-pirakitti@users.noreply.github.com>
2025-03-22 13:29:53 -0500

    Bug fixes, refactoring.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,9 @@ const apiBaseUrl = "http://localhost:3000";
 
 function App() {
     const [plants, setPlants] = useState([]);
-    const [currentPlant, setCurrentPlant] = useState({});
+    const [currentPlant, setCurrentPlant] = useState(null);
     const [isShowingPlantInfo, setIsShowingPlantInfo] = useState(false);
+    const [currentFilter, setCurrentFilter] = useState("");
     const plantInfo = useRef();
 
     useEffect(() => {
@@ -27,61 +28,128 @@ function App() {
                     allPlants.push(plant);
                 }
             }
+            console.log(`Got ${allPlants.length} plants.`);
             setPlants(allPlants);
-            return allPlants;
-          }).then((allPlants) => {
-            showRandom(allPlants);
           });
       }, []);
 
-    function showRandom(plantList) {
+    useEffect(() => {
+        if (plants.length === 0) {
+            console.log("No plants (1)");
+            return;
+        }
         setIsShowingPlantInfo(false);
-        const index = Math.floor(Math.random() * plantList.length);
-        setCurrentPlant(plantList[index]);
-    }
+        setRandomPlant();
+    }, [plants, currentFilter]);
 
     function showPlantInfo(){
         setIsShowingPlantInfo(true);
     }
 
-    function removeCurrent() {
+    function filter(selectedFilter){
+        setCurrentFilter(selectedFilter);  
+    }
+
+    function getFilteredPlants(groupName) {
+        return plants.filter(p =>p.group === groupName);
+    }
+
+    function setRandomPlant() {
+        let newPlant = null;
+        if (currentFilter === "" || currentFilter === undefined || currentFilter === null) {
+            const index = Math.floor(Math.random() * plants.length);
+            newPlant = plants[index];
+        }
+        else {
+            const filteredPlants = getFilteredPlants(currentFilter);
+            if (filteredPlants.length === 0) {
+                console.log("No filtered plants (1)");
+                return;
+            }            
+            
+            const filteredIndex = Math.floor(Math.random() * filteredPlants.length);
+            newPlant = filteredPlants[filteredIndex];
+        }
+        
+        setCurrentPlant(newPlant);
+    }
+
+    function handleAdvance() {
+        setRandomPlant();
+    }
+
+    function handleCorrect() {
+        /*
+        // warning!  two effects here        
+        setPlants(plants.filter(p => p.name !== currentPlant.name));
+        const newIndex = getRandomIndex();
+        setCurrentIndex(newIndex);
+        */
+
+        /*
+        // error:
+        // State updates from the useState() and useReducer() Hooks don't support the second callback argument. 
+        // To execute a side effect after rendering, declare it in the component body with useEffect().
+        setPlants(
+            plants.filter(p => p.name !== currentPlant.name),
+            () => {
+                const newIndex = getRandomIndex();
+                setCurrentIndex(newIndex);
+            }
+        ); 
+        */
+       
         setPlants(plants.filter(p => p.name !== currentPlant.name));
     }
 
-    function filter(plants, selection){
-        if (selection === "") {
-            showRandom(plants);    
-        }
-        else {
-            showRandom(plants.filter(p => p.group === selection));
-        }
-    }
-
     return <>
-            { plants.length > 0 && (
-                <div id="card-wrapper">
-                    <button onClick={() => {showRandom(plants);}}>Advance</button>
-                    <button onClick={() => {removeCurrent();showRandom(plants);}}>Correct</button>
-                    <select name="groupFilter" onChange={(e) => filter(plants, e.target.value)}>
-                        <option key="None" name="">None ({plants.length})</option>
-                        {
-                            Array.from(new Set(plants.map(p=>p.group))).map(groupName => 
-                                <option key={groupName} value={groupName}>{groupName}&nbsp;({plants.filter(p =>p.group === groupName).length})</option>
-                            )
-                        }
-                    </select>
-                    <div className={"plant-info" + (isShowingPlantInfo ? " unhidden" : "")} onClick={showPlantInfo}>
-                        <p>{currentPlant.name}</p>
-                        <em>{currentPlant.species}</em>
-                        <p>{currentPlant.bloomTime}</p>            
-                    </div>
-                    <img className="plant-image" src={currentPlant.imageUrl} ref={plantInfo} />
-                    <div>{plants.length} left</div>
-                </div>
+            {                 
+                plants.length === 0 ? (
+                    <div>All done!</div>
+                )                
+                : currentFilter !== "" && getFilteredPlants(currentFilter).length === 0 ? (
+                    <div>All done with current filter.  Choose another group to continue: 
+                        <select name="groupFilter" onChange={(e) => filter(e.target.value)}>
+                            <option key="Unselected" name=""></option>
+                            <option key="None" name="">None ({plants.length})</option>
+                            {
+                                Array.from(new Set(plants.map(p=>p.group))).map(groupName => 
+                                    <option key={groupName} value={groupName}>{groupName}&nbsp;({getFilteredPlants(groupName).length})</option>
+                                )
+                            }
+                        </select>
+                    </div>                    
                 )
-            }
-            { plants.length == 0 && (
-                <div>All done!</div>
+                : currentPlant === null ? (
+                    <p>Loading...</p>
+                )
+                : (
+                    <div id="card-wrapper">
+                        <button onClick={handleAdvance}>Advance</button>
+                        <button onClick={handleCorrect}>Correct</button>
+                        <select name="groupFilter" onChange={(e) => filter(e.target.value)}>
+                            <option key="None" name="">None ({plants.length})</option>
+                            {
+                                Array.from(new Set(plants.map(p=>p.group))).map(groupName => 
+                                    <option key={groupName} value={groupName}>{groupName}&nbsp;({getFilteredPlants(groupName).length})</option>
+                                )
+                            }
+                        </select>
+                        <div className={"plant-info" + (isShowingPlantInfo ? " unhidden" : "")} onClick={showPlantInfo}>
+                            <p>{currentPlant.name}</p>
+                            <em>{currentPlant.species}</em>
+                            <p>{currentPlant.bloomTime}</p>            
+                        </div>
+                        <img className="plant-image" src={currentPlant.imageUrl} ref={plantInfo} />
+                        <div>
+                            {
+                                currentFilter == "" ?
+                                    <span>{plants.length} left</span> 
+                                    : <span>{getFilteredPlants(currentFilter).length} left in group | {plants.length} total left</span>
+                            }
+                            
+                        </div>
+                    </div>
                 )
             }
         </>
